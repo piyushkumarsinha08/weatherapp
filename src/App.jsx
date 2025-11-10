febd83ab1579f8cc48ea99a2d7af9dc342cd3c2d
piyushkumarsinha08 <sinhapiyush275@gmail.com>
2025-11-10 11:28:04 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import WeatherApp from '../components/weatherapp'
+import Card from '../components/card'
+
+
+function App() {
+return (    
+<section className="flex flex-col">
+<WeatherApp/>
+</section>
+
+  )
+}
+
+export default App
